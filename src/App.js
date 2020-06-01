5e2e13b60a501ac49abce7b56fe9047830be9641
Everton Zanella Alvarenga <eza@protonmail.ch>
2020-06-01 18:19:16 +0200

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,72 +1,68 @@
-import React from 'react';
-import result from './data.json'
-import InputField from './InputField'
-import SearchButton from './SearchButton'
-import ShowResults from './ShowResults'
-import Grid from '@material-ui/core/Grid'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import SwitchStyle from './SwitchStyle';
+import React from "react";
+import result from "./data.json";
+import InputField from "./InputField";
+import SearchButton from "./SearchButton";
+import ShowResults from "./ShowResults";
+import Grid from "@material-ui/core/Grid";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import SwitchStyle from "./SwitchStyle";
 
 const theme = createMuiTheme({
-  palette:{
+  palette: {
     primary: {
-      main:'#007BA7',
+      main: "#007BA7",
     },
-    secondary:{
-      main:'#02e2f2'
-    }
+    secondary: {
+      main: "#02e2f2",
+    },
   },
   typography: {
     fontFamily: [
-      'Nunito',
-      'Roboto',
+      "Nunito",
+      "Roboto",
       '"Helvetica Neue"',
-      'Arial',
-      'sans-serif'
-    ].join(','),
-  }
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
 });
 
 class App extends React.Component {
-  state={
-    inputSphere:0,
-    results:[],
+  state = {
+    inputSphere: 0,
+    results: [],
     darkMode: false,
-  }
-  findResults=(inputSphere)=>{
-    this.setState({results:result.data})
-  }
-  onSearch=()=>{
-    console.log("to be done")
-  }
-  onChange=(key,value)=>{
-    this.setState({[key]:value})
-  }
-  toggledarkMode=()=>{
+  };
+  findResults = (inputSphere) => {
+    this.setState({ results: result.data });
+  };
+  onSearch = () => {
+    console.log("to be done");
+  };
+  onChange = (key, value) => {
+    this.setState({ [key]: value });
+  };
+  toggledarkMode = () => {
     this.setState({
-      darkMode: !this.state.darkMode
-    })
-  }
-  render (){
+      darkMode: !this.state.darkMode,
+    });
+  };
+  render() {
     const { darkMode, inputSphere, results } = this.state;
-    return(
+    return (
       <ThemeProvider theme={theme}>
-        <div className={darkMode?"AppDark":"AppLight"}>
-          <Grid className='searchField'>
-          <InputField
-            inputField={inputSphere}
-            name="inputSphere"
-            label="Sphere"
-            onChange={this.onChange}
-          />
-          <SearchButton
-            onSearch={this.onSearch}
-          />
+        <div className={darkMode ? "AppDark" : "AppLight"}>
+          <Grid className="searchField">
+            <InputField
+              inputField={inputSphere}
+              name="inputSphere"
+              label="Sphere"
+              onChange={this.onChange}
+            />
+            <SearchButton onSearch={this.onSearch} />
           </Grid>
-          <Grid className={darkMode?'serchResult':'serchResultLight'}>
-          <ShowResults
-            results={results}
-          />
+          <Grid className={darkMode ? "serchResult" : "serchResultLight"}>
+            <ShowResults results={results} />
           </Grid>
           <SwitchStyle
             darkMode={this.state.darkMode}
@@ -74,7 +70,7 @@ class App extends React.Component {
           />
         </div>
       </ThemeProvider>
-    )
+    );
   }
 }
 
